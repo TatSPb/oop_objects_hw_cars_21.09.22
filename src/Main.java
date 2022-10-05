140e4d5009497b2266402e1eaa4ef768435274f6
TatSPb <t-sverchkova@yandex.ru>
2022-10-05 23:11:46 +0300

    Hw-human-part-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human maksim = new Human(); //new = создать новый объект типа/класса Cat
+        maksim.name = "Максим";
+        maksim.yearOfBirth = 1987;
+        maksim.cityOfResidence = "Минск";
+        maksim.position = "бренд-менеджера";
+        maksim.hello();
+
+        Human anya = new Human(); //new = создать новый объект типа/класса Cat
+        anya.name = "Аня";
+        anya.yearOfBirth = 1993;
+        anya.cityOfResidence = "Москва";
+        anya.position = "методиста образовательных программ";
+        anya.hello();
+
+        Human katya = new Human(); //new = создать новый объект типа/класса Cat
+        katya.name = "Катя";
+        katya.yearOfBirth = 1994;
+        katya.cityOfResidence = "Калининград";
+        katya.position = "продакт-менеджера";
+        katya.hello();
+
+        Human artem = new Human(); //new = создать новый объект типа/класса Cat
+        artem.name = "Артем";
+        artem.yearOfBirth = 1995;
+        artem.cityOfResidence = "Москва";
+        artem.position = "директора по развитию бизнеса";
+        artem.hello();
     }
 }
