7f94ae7e6ecceb00497a7914022e754370951eab
TatSPb <t-sverchkova@yandex.ru>
2022-10-07 23:59:04 +0300

    Hw_human+flower_incapsulation_part_1

--- src/Human.java
+++ src/Human.java
@@ -1,25 +1,75 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.time.LocalDate;
+
 public class Human {
+
     String name;
-    int yearOfBirth;
-    String cityOfResidence;
-    String job;
+    private String cityOfResidence;
+    private String job;
+    private Integer yearOfBirth;
+    //private Integer age;
 
-    Human(int yearOfBirth) {
-        this(yearOfBirth, "<Информация не указана>", "<Информация не указана>", "<Информация не указана>");
+    /*** NAME ***/
+    public String getName() {
+        return name;
     }
-
-    Human(int yearOfBirth, String name, String cityOfResidence, String job) {
-        if (yearOfBirth >= 0) {
-            this.yearOfBirth = yearOfBirth;
+    public void setName(String name) {
+        if (name != null && !name.isEmpty() && !name.isBlank()) {
+            this.name = name;
         } else {
-            yearOfBirth = Math.abs(yearOfBirth);
+            this.name = "<Информация не указана>";
         }
-        this.name = name;
-        this.cityOfResidence = cityOfResidence;
-        this.job = job;
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + cityOfResidence + ". Я родился (родилась) в "
-                + yearOfBirth + " году. Я работаю в должности " + job + ". Будем знакомы!");
     }
-}
+    /*** JOB ***/
+    public void setJob(String job) {
+        //this.job = job;
+        if (job != null && !job.isEmpty() && !job.isBlank()) {
+            this.job = job;
+        } else {
+            this.job = "<Информация не указана>";
+        }
+    }
+    public String getJob() {
+        return job;
+    }
+
+    /*** AGE ***/
+        public void setAge(Integer age) {
+        if (age >= 0 && age != null) {
+            this.yearOfBirth = LocalDate.now().getYear() - age;
+        } else {
+            this.yearOfBirth = LocalDate.now().getYear();
+        }
+        }
+        public int getAge () {
+            return LocalDate.now().getYear() - yearOfBirth;
+        }
+
+        /*** CITY OF RESIDENCE ***/
+        public void setCityOfResidence(String cityOfResidence){
+            if (cityOfResidence != null && !cityOfResidence.isEmpty() && !cityOfResidence.isBlank()) {
+                this.cityOfResidence = cityOfResidence;
+            } else {
+                this.cityOfResidence = "<Информация не указана>";
+            }
+        }
+        public String getCityOfResidence () {
+                return cityOfResidence;
+            }
+
+
+        /*** КОНСТРУКТОР ***/
+        Human(Integer age){
+            this(age, "<Информация не указана>", "<Информация не указана>", "<Информация не указана>");
+        }
+
+        Human(Integer age, String name, String cityOfResidence, String job){
+            this.setName(name);
+            this.setCityOfResidence(cityOfResidence);
+            this.setJob(job);
+            this.setAge(age);
+            System.out.println("Привет! Меня зовут " + getName() + ". Я из города " + getCityOfResidence() + ". Мне " + getAge() + " лет (год). " +
+                    "Я родился (родилась) в " + yearOfBirth + " году. Я работаю в должности " + getJob() + ". Будем знакомы!");
+        }
+    }
